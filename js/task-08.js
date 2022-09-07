2e502cf9bb20b2f7dd2cdec89d987b987e607a9a
Victor Grossu <weekgrossu@gmail.com>
2022-09-07 14:06:09 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+
+  const userData = {
+    email: `${email.name}: ${email.value}`,
+    password: `${password.name}: ${password.value}`,
+  };
+  console.log(userData);
+
+  event.currentTarget.reset();
+});
